0e21ae6d4801f28a40842651e8c0310d924c3eab
Yannick fumukani <slashcs7@hotmail.com>
2025-04-24 21:12:43 -0400

    MongoDb added to SearchService

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,10 +1,14 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllers();
-
+// builder.Services.
 
 var app = builder.Build();
 
@@ -12,5 +16,14 @@ var app = builder.Build();
 
 // app.UseHttpsRedirection();
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
